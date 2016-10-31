dc6102185a4f86701fe3852109bf2184f1757c04
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-10-30 22:39:16 -0200

    Implementing pull-to-refresh

--- www/js/app.js
+++ www/js/app.js
@@ -30,6 +30,13 @@ angular.module('starter', ['ionic'])
       item.star = !item.star;
     };
 
+    $scope.doRefresh = function() {
+      $http.get('js/data.json').success(function(data){
+        $scope.artists = data;
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    };
+
     $scope.onItemDelete = function(item) {
       $scope.artists.splice($scope.artists.indexOf(item), 1);
     };
